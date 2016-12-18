873e0d6ee11d26cf51922ccea81f1db3fc86bfc3
Nathaniel Hoag <info@nathanielhoag.com>
2016-12-18 13:54:36 -0500

    Add DB URL

--- scripts/travis/run-tests.sh
+++ scripts/travis/run-tests.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # execute Drupal tests
-php docroot/core/scripts/run-tests.sh --php `which php` --verbose --color --url http://localhost
+php docroot/core/scripts/run-tests.sh --php `which php` --verbose --color --url http://localhost --db-url=mysql://root:@localhost/drupal
