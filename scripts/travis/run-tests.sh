ef2d2b6987ab6efba7be80ce404ee1f0102a90d0
Nathaniel Hoag <info@nathanielhoag.com>
2016-12-18 13:58:57 -0500

    Correct DB URL

--- scripts/travis/run-tests.sh
+++ scripts/travis/run-tests.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # execute Drupal tests
-php docroot/core/scripts/run-tests.sh --php `which php` --verbose --color --url http://localhost --dburl=mysql://root:@localhost/drupal
+php docroot/core/scripts/run-tests.sh --php `which php` --verbose --color --url http://localhost --dburl mysql://root:@localhost/drupal
