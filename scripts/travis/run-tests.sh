a18b12d59ead8e9cd89d6ec85e53b01af827b517
Nathaniel Hoag <info@nathanielhoag.com>
2016-12-18 22:36:34 -0500

    x

--- scripts/travis/run-tests.sh
+++ scripts/travis/run-tests.sh
@@ -5,6 +5,6 @@ php docroot/core/scripts/run-tests.sh \
   --php `which php` --verbose --color \
   --url http://localhost \
   --dburl mysql://root:@localhost/drupal \
-  --directory docroot/modules/custom
+  --directory modules/custom
 
 # phpunit docroot/modules/custom
