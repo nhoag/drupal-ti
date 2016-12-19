50fbb223ec7506cd53643b6ca12ec5cb7384742b
Nathaniel Hoag <info@nathanielhoag.com>
2016-12-18 22:07:04 -0500

    Use phpunit

--- scripts/travis/run-tests.sh
+++ scripts/travis/run-tests.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 # execute Drupal tests
-php docroot/core/scripts/run-tests.sh --php `which php` --verbose --color --url http://localhost --dburl mysql://root:@localhost/drupal --directory docroot/modules/custom
+# php docroot/core/scripts/run-tests.sh --php `which php` --verbose --color --url http://localhost --dburl mysql://root:@localhost/drupal --directory docroot/modules/custom
+
+phpunit docroot/modules/custom
