fcb54f6351d06ec633b85b59975e3c2015d9d950
Nathaniel Hoag <info@nathanielhoag.com>
2016-12-19 22:06:16 -0500

    Add phpcs

--- scripts/travis/test-source-code.sh
+++ scripts/travis/test-source-code.sh
@@ -3,6 +3,9 @@ phpcs --config-set installed_paths ~/.composer/vendor/drupal/coder/coder_sniffer
 phpcs --standard=Drupal --extensions=php,module,inc,install,test,profile,theme --report=summary -p ./docroot/modules/custom
 phpcs --standard=DrupalPractice --extensions=php,module,inc,install,test,profile,theme --report=summary -p ./docroot/modules/custom
 
+# Run PHPMD
+phpmd ./docroot/modules/custom text cleancode,codesize,naming,unusedcode,design
+
 # JS ESLint checking
 mv ~/.nvm ~/.nvm-backup
 git clone https://github.com/creationix/nvm.git ~/.nvm
